dbfc673f7dec99f6b2665851906f8572f107029a
Joost Rovers <joost@rovers.pt>
2021-12-28 15:42:38 +0000

    dashboard resources now use module prefix instead of dashboard name

--- aws/src/dashboard.tf
+++ aws/src/dashboard.tf
@@ -3,52 +3,52 @@
 ##############################
 
 resource "aws_cloudwatch_log_metric_filter" "jobs_started" {
-  name           = "${var.dashboard_name}-type-JOB_START"
+  name           = "${var.prefix}-jobs-started"
   pattern        = "{ $.type = \"JOB_START\" }"
   log_group_name = var.log_group.name
 
   metric_transformation {
     name          = "JobsStarted"
-    namespace     = var.dashboard_name
+    namespace     = var.prefix
     value         = "1"
     default_value = "0"
   }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "jobs_completed" {
-  name           = "${var.dashboard_name}-message-jobStatus-Completed"
+  name           = "${var.prefix}-jobs-completed"
   pattern        = "{ $.type = \"JOB_END\" && $.message.jobStatus = \"Completed\" }"
   log_group_name = var.log_group.name
 
   metric_transformation {
     name          = "JobsCompleted"
-    namespace     = var.dashboard_name
+    namespace     = var.prefix
     value         = "1"
     default_value = "0"
   }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "jobs_failed" {
-  name           = "${var.dashboard_name}-message-jobStatus-Failed"
+  name           = "${var.prefix}-jobs-failed"
   pattern        = "{ $.type = \"JOB_END\" && $.message.jobStatus = \"Failed\" }"
   log_group_name = var.log_group.name
 
   metric_transformation {
     name          = "JobsFailed"
-    namespace     = var.dashboard_name
+    namespace     = var.prefix
     value         = "1"
     default_value = "0"
   }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "jobs_canceled" {
-  name           = "${var.dashboard_name}-message-jobStatus-Canceled"
+  name           = "${var.prefix}-jobs-canceled"
   pattern        = "{ $.type = \"JOB_END\" && $.message.jobStatus = \"Canceled\" }"
   log_group_name = var.log_group.name
 
   metric_transformation {
     name          = "JobsCanceled"
-    namespace     = var.dashboard_name
+    namespace     = var.prefix
     value         = "1"
     default_value = "0"
   }
@@ -58,7 +58,7 @@ resource "aws_cloudwatch_dashboard" "dashboard" {
   dashboard_name = "${var.dashboard_name}-${var.aws_region}"
   dashboard_body = templatefile("${path.module}/dashboard.json", {
     aws_region     = var.aws_region
-    dashboard_name = var.dashboard_name
+    namespace      = var.prefix
     log_group_name = var.log_group.name
   })
 }
